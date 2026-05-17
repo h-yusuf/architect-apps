6ed17797796ac548c2ed62ecaed2b84b52d277cd
h-yusuf <hafidhy288@gmail.com>
2026-05-17 13:16:14 +0700

    feat: wire up all routes including admin panel

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,11 @@ import { Routes, Route } from "react-router-dom";
 import { signInAnonymously } from "firebase/auth";
 import { auth } from "./firebase";
 import Home from "./pages/Home";
-import Projects from "./pages/Projects";
-import About from "./pages/About";
+import DocDetail from "./pages/DocDetail";
+import AdminLogin from "./pages/AdminLogin";
+import AdminDashboard from "./pages/AdminDashboard";
+import AdminEditDoc from "./pages/AdminEditDoc";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   useEffect(() => {
@@ -14,8 +17,20 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/projects" element={<Projects />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/doc/:slug" element={<DocDetail />} />
+      <Route path="/admin" element={<AdminLogin />} />
+      <Route
+        path="/admin/dashboard"
+        element={<ProtectedRoute><AdminDashboard /></ProtectedRoute>}
+      />
+      <Route
+        path="/admin/doc/new"
+        element={<ProtectedRoute><AdminEditDoc /></ProtectedRoute>}
+      />
+      <Route
+        path="/admin/doc/:id/edit"
+        element={<ProtectedRoute><AdminEditDoc /></ProtectedRoute>}
+      />
     </Routes>
   );
 }
